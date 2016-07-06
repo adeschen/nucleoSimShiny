7e3bfbe18fba8ce2f9272d771548ef0362427747
adeschen <adeschen@hotmail.com>
2016-07-06 17:12:05 -0400

    Adding navigation bar; updating vignette

---
+++ R/shiny/about.md
@@ -0,0 +1,5 @@
+
+The **shinyNucleoSim** application is based on the package 
+**nucleoSim**, available at Bioconductor at this 
+address: [http://bioconductor.org/packages/nucleoSim/](http://bioconductor.org/packages/nucleoSim/).
+
--- R/shiny/ui.R
+++ R/shiny/ui.R
@@ -5,14 +5,15 @@ library(shinythemes)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   theme = shinytheme("journal"),
-                 
+  navbarPage("shinyNucleoSim",
+    tabPanel("Simulation",
   # Application title
   titlePanel(h1("Interactive Generator of Synthetic Nucleosome Maps")),
   
   # Sidebar with a slider input for the number of bins
   fluidRow(
       column(4,
-      h2("Well-positioned nucleosomes"),
+      h2("Good nucleosomes"),
       
       sliderInput("wellNucl",
                   "Number of nucleosomes:",
@@ -60,6 +61,18 @@ shinyUI(fluidPage(
     # Show a plot of the generated distribution
     
     plotOutput("distPlot")
-    
-  )
+    )),
+    tabPanel("About",
+            fluidRow(
+               column(6,
+                      includeMarkdown("about.md")
+               ),
+               column(3,
+                      tags$small(
+                        "TODO",
+                        "TODO")
+                      )
+               )
+            )
+    )
 ))
