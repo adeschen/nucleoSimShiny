d82b90bef9c2e63bc462eb1cc5c10e2002271a1a
adeschen <adeschen@hotmail.com>
2016-08-03 12:16:02 -0400

    Adding text and changing interface on simulation panel.

--- R/shiny/ui.R
+++ R/shiny/ui.R
@@ -5,22 +5,21 @@ library(DT)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   theme = shinytheme("journal"),
-  navbarPage("shinyNucleoSim",
+  navbarPage(div("Interactive Generator of Synthetic Nucleosomes", style= "color:midnightblue"),
     tabPanel("Simulation",
   # Application title
-  titlePanel(h1("Interactive Generator of Synthetic Nucleosome Sample")),
-  titlePanel(h4("Nucleosomes play a critical role in regulating the process 
+  h2(div("Synthetic Nucleosome Parameters", style = "color:steelblue")),
+  h4(div("Nucleosomes play a critical role in regulating the process 
                 of gene transcription. Several software have been developed 
                 to detect the position of the nucleosomes on the genome using
                 paired-end reads. 
                 Simulated nucleosome samples are useful tools to
-                test the sensitivity and the accurancy of those software.")),
+                test the sensitivity and the accurancy of those software.", style = "color:gray")),
   
   # Sidebar with a slider input for the number of bins
   fluidRow(
       column(4,
-      h2("Good nucleosomes"),
-      
+      h3("Good nucleosomes"),
       sliderInput("wellNucl",
                   "Number of nucleosomes:",
                   min = 1,
@@ -38,8 +37,7 @@ shinyUI(fluidPage(
 #                   value = 1)
       ),
       column(4,
-      h2("Fuzzy nucleosomes"),
-      
+      h3("Fuzzy nucleosomes"),
       sliderInput("fuzNucl",
                   "Number of nucleosomes:",
                   min = 1,
@@ -52,20 +50,23 @@ shinyUI(fluidPage(
                   value = 10)
       ),
       column(4,
-             h2("Global parameters"),
-             
-             sliderInput("maxCoverage",
-                         "Maximum coverage:",
-                         min = 10,
-                         max = 300,
-                         value = 100),
-             numericInput("offset", label = "Offset:", value = 1000, 
-                          min = 0, step=100)
-      ),
-    hr(),
+      h3("Global parameters"),
+      sliderInput("maxCoverage",
+                  "Maximum coverage:",
+                  min = 10,
+                  max = 300,
+                  value = 100),
+      numericInput("offset", label = "Offset:", value = 1000, 
+                  min = 0, step=100)
+      )
+    ),
+    h2(div("Synthetic Nucleosome Map", style = "color:steelblue")),
+    h4(div("Good nucleosomes are positionned every 147 base pairs  (green dots). The starting
+      position of the first good nucleosome correspond to the selected offset + 147. Fuzzy nucleosomes are 
+      randomnly distributed (red dots).", style = "color:gray")),
     # Show a plot of the nucleosome plot
     plotOutput("distPlot")
-    )),
+    ),
     tabPanel("Download",
              br(),
              downloadButton('downloadData', 'Download'),
@@ -76,8 +77,8 @@ shinyUI(fluidPage(
              #tableOutput('table')
     ),
     tabPanel("About",
+            br(),
             fluidRow(
-               br(),
                column(4,
                       img(src='logo_bioconductor.gif')
                ),
