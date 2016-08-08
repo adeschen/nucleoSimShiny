ee3643ad544a28da51b786a6924b04c4c4354ff0
adeschen <adeschen@hotmail.com>
2016-08-08 16:16:29 -0400

    Changing markdown location. Creating new markdown file.

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -5,18 +5,15 @@ library(DT)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   theme = shinytheme("journal"),
-  navbarPage(div("Interactive Generator of Synthetic Nucleosomes", style= "color:midnightblue"),
+  navbarPage(div("Interactive Generator of Synthetic Nucleosomes",
+                 style= "color:midnightblue"),
     tabPanel("Simulation",
-  # Application title
-  h2(div("Synthetic Nucleosome Parameters", style = "color:steelblue")),
-  h4(div("Nucleosomes play a critical role in regulating the process 
-                of gene transcription. Several software have been developed 
-                to detect the position of the nucleosomes on the genome using
-                paired-end reads. 
-                Simulated nucleosome samples are useful tools to
-                test the sensitivity and the accurancy of those software.", style = "color:gray")),
-  
-  # Sidebar with a slider input for the number of bins
+      ####################################
+      # Application description
+      ####################################
+      h4(div(includeMarkdown("www/nucleosomesDescription.md"), style = "color:gray")),
+      # Parameters section
+      h2(div("Synthetic Nucleosome Parameters", style = "color:steelblue")),
   fluidRow(
       column(4,
       h3("Good nucleosomes"),
@@ -60,10 +57,11 @@ shinyUI(fluidPage(
                   min = 0, step=100)
       )
     ),
+    ####################################
+    # Graph
+    ####################################
     h2(div("Synthetic Nucleosome Map", style = "color:steelblue")),
-    h4(div("Good nucleosomes are positionned every 147 base pairs  (green dots). The
-      position of the first good nucleosome correspond to the selected offset + 147 bases. Fuzzy nucleosomes are 
-      randomnly distributed (red dots).", style = "color:gray")),
+    h4(div(includeMarkdown("www/nucleosomesGraph.md"), style = "color:gray")),
     # Show a plot of the nucleosome plot
     plotOutput("distPlot")
     ),
---
+++ inst/shiny/www/nucleosomesDescription.md
@@ -0,0 +1,5 @@
+Nucleosomes play a critical role in regulating the process of gene 
+transcription. Several software have been developed to detect the position of 
+the nucleosomes on the genome using paired-end reads. Simulated nucleosome 
+samples are useful tools to test the sensitivity and the accurancy of 
+those software.
